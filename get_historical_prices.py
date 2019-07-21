9711d25549a1c432ba1a7ef38859fa855e62e08b
rwuebker <rlwuebker@gmail.com>
2019-07-20 21:24:18 -0700

    edits index for historical prices

--- get_historical_prices.py
+++ get_historical_prices.py
@@ -3,11 +3,11 @@ import pandas as pd
 from tqdm import tqdm
 
 class HistoricalPrices:
-    def __init__(self, start_date=None, num_assets=967, num_days=None):
+    def __init__(self, start_date=None, num_assets=967, num_days=None, prices_dir='prices'):
         self.num_days = num_days
         self.num_assets = num_assets
         self.start_date = start_date
-        self.price_dir = 'prices'
+        self.price_dir = prices_dir
 
     def get_prices(self):
         prices = pd.DataFrame()
@@ -16,8 +16,7 @@ class HistoricalPrices:
             prices = prices.append(pd.read_csv(filename))
 
         prices.columns = ['ticker', 'date', 'high', 'low', 'adjclose', 'volume']
-        self.prices = prices.set_index('date')
-
+        prices = prices.set_index(['ticker', 'date'])
 
 
 if __name__ == '__main__':
