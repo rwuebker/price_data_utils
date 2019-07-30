b7fcb1c3393d03044eab5b20023c89ec0adc0b1e
rwuebker <rlwuebker@gmail.com>
2019-07-29 17:23:31 -0700

    data_loader now caches and looks for cached files first

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import json
 import numpy as np
 import os
 import pandas as pd
@@ -9,12 +10,19 @@ from tqdm import tqdm
 
 class DataLoader:
 
-    def __init__(self, analysis_date_str=dt.date.today().strftime('%Y-%m-%d'), prices_dir='prices', info_dir='info', periods=1):
+    def __init__(self, analysis_date_str=dt.date.today().strftime('%Y-%m-%d'),
+                 prices_dir='prices', info_dir='info', periods=1, cached_dir='cached'):
         # prev date is decision date, trade date is the price we get
+        self.cached_dir = cached_dir
         self.analysis_date_str = analysis_date_str
         self.analysis_date = dt.datetime.strptime(analysis_date_str, '%Y-%m-%d')
+        self.periods = periods
+        self.date_sets = {}
+        self.cached = self.check_cached()
+
         self.pred_date = self.analysis_date + BDay(2)
         self.pred_date_str = self.pred_date.strftime('%Y-%m-%d')
+        print('Prediction Date: {}'.format(self.pred_date_str))
         self.prev_date = self.analysis_date - BDay(periods)
         self.prev_date_str = self.prev_date.strftime('%Y-%m-%d')
         self.trade_date = self.analysis_date + BDay(1)
@@ -25,12 +33,38 @@ class DataLoader:
         self.month_ago_str = self.month_ago.strftime('%Y-%m-%d')
         self.prices_dir = prices_dir
         self.info_dir = info_dir
-        self.date_sets = {}
+
+    def check_cached(self):
+        cached_path = '{}/data/data_{}_periods_{}.csv'.format(self.cached_dir, self.analysis_date_str, self.periods)
+        date_sets_path = '{}/date_sets/date_set_{}_periods_{}.csv'.format(self.cached_dir, self.analysis_date_str, self.periods)
+        if os.path.exists(cached_path) and os.path.exists(date_sets_path):
+            df = pd.read_csv(cached_path, index_col=0)
+            self.data = df
+            self.data.index.names = ['ticker']
+
+            with open(date_sets_path, 'r') as f:
+                self.date_sets = json.load(f)
+            return True
+        return False
+
+    def _cache_items(self):
+        cached_path = '{}/data/data_{}_periods_{}.csv'.format(self.cached_dir, self.analysis_date_str, self.periods)
+        date_sets_path = '{}/date_sets/date_set_{}_periods_{}.csv'.format(self.cached_dir, self.analysis_date_str, self.periods)
+        if not os.path.exists('{}/data'.format(self.cached_dir)):
+            os.mkdirs('{}/data'.format(self.cached_dir))
+        self.data.to_csv(cached_path)
+        if not os.path.exists('{}/date_sets'.format(self.cached_dir)):
+            os.mkdirs('{}/date_sets'.format(self.cached_dir))
+        with open(date_sets_path, 'w') as f:
+            json.dump(self.date_sets, f, indent=4, sort_keys=True, default=str)
 
     def load_data(self):
-        self._load_info()
-        self._load_prices()
-
+        if self.cached:
+            return
+        else:
+            self._load_info()
+            self._load_prices()
+            self._cache_items()
 
     def _load_info(self):
         # use last business days date for info
@@ -44,12 +78,11 @@ class DataLoader:
                     df.set_index('ticker', inplace=True)
                     df.drop(['cur_price', 'prev_price'], axis=1, inplace=True)
                     totals = totals.append(df)
-
         self.info = totals
 
     def _load_prices(self):
         path = '{}/prices_{}'.format(self.prices_dir, self.pred_date_str)
-        path = '{}/prices_{}'.format(self.prices_dir, '2019-07-26')
+        #path = '{}/prices_{}'.format(self.prices_dir, '2019-07-26')
         info = self.info
         totals = pd.DataFrame()
         prices = pd.DataFrame()
@@ -60,22 +93,16 @@ class DataLoader:
                     self._initialize_date_set(ticker)
                     full_file_name = '{}/{}'.format(path, filename)
                     df = pd.read_csv(full_file_name, index_col='Date')
-                    temp_df = self._load_price_specific_dates(df)
-                    totals = totals.append(new_df)
-
-                    df['ticker'] = ticker
-                    df.set_index(['ticker', 'Date'], inplace=True)
+                    temp_df = self._load_price_specific_dates(df, ticker)
+                    totals = totals.append(temp_df)
                     prices = prices.append(df)
 
-            final = pd.merge(info, totals, left_index=True, right_index=True)
+            self.data = pd.merge(info, totals, left_index=True, right_index=True)
         else:
             print('path doesnt exist: ')
             print(path)
 
-        self.info = final
-        print(final.head())
-
-    def _load_price_specific_dates(self, df):
+    def _load_price_specific_dates(self, df, ticker):
         data = {
                 "month_ago": self._get_value(ticker, df, 'month_ago', 'Adj Close'),
                 "year_ago": self._get_value(ticker, df, 'year_ago', 'Adj Close'),
@@ -134,5 +161,6 @@ class DataLoader:
 
 
 if __name__ == '__main__':
-    fa = FactorAttribution(analysis_date_str='2019-07-24')
-    fa.run_attribution()
+    fa = DataLoader(analysis_date_str='2019-07-25')
+    fa.load_data()
+    print(fa.data.head())
